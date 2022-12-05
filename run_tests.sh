1c705a24bd594dcc7d2833be6147dec29ace64a6
sjcjjcjssdssjc <1339590461@qq.com>
2022-12-05 13:52:55 +0800

    cpu: day17
    Change-Id: Ifbb9efa0934a5afa0e5bd7e51cf9e5766e826be2

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,7 @@
-mydir = '~/gem5_plctlab/configs/example/se.py' 
-cd '~/riscv-tests/isa' 
-for FILE in rv64u*-p-*[^dump]; do ~/gem5_plctlab/build/RISCV/gem5.opt $mydir -c ./$FILE done
+#!/bin/bash
+mydir=~/gem5_plctlab/configs/example/se.py
+cd ~/riscv-tests/isa
+for FILE in rv64u*-p-*[^dump]; do ~/gem5_plctlab/build/RISCV/gem5.opt $mydir -c ./$FILE; done
+
+
+#  /home/outlander/gem5_plctlab/build/RISCV/gem5.opt --debug-flags=Fetch,Decode,Rename,IEW,Commit /home/outlander/gem5_plctlab/configs/example/se.py -c '/home/outlander/riscv-tests/isa/rv32ui-p-addi'
